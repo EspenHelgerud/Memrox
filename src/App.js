618eabc3896668113bd0dfb6715a037a6b868d08
EspenHelgrud <Kompa95@live.no>
2025-02-08 10:04:36 +0100

    Add logo and "under construction" text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import logo from './assets/images/logo/MemroxLogo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className='logo' alt="Logo"/>
         <p>
-          MEMROX
+          UNDER CONSTRUCTION...
         </p>
       </header>
     </div>
